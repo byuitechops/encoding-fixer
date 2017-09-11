89cbbeff8ba07367a0232ab3eea1e3802cf0d431
Joshua McKinney <mckinneyj@byui.edu>
2017-09-11 12:59:33 -0600

    all Latin chars

--- readfile.js
+++ readfile.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.readFile("L1MC.csv", function (err, data) {
+fs.readFile("./csvFiles/MC CSV files/csv files/L1MC.csv", function (err, data) {
     if (err) {
         console.error(err);
         return;
@@ -23,29 +23,45 @@ fs.readFile("L1MC.csv", function (err, data) {
 
 
     var nastyChars = hexArray
-    //conver the hex to base 10 (ascii)
-    .map(function (hex) {
-        return parseInt(hex, 16);
-    })
-    //filter out know acceptable characters (ascii)
-    .filter(function (hex) {
-        if ((hex >= 65 && hex <= 90) || (hex >= 97 && hex <= 122) || (hex >= 48 && hex <= 57) || (hex >= 91 && hex <= 95)) {
-            return false;
-        } else {
-            return true;
-        }
-    })
-    //unique the results
-    .filter(function(item, i, daList){
-        return i == daList.indexOf(item);
-    })
-    //convert value back to to base 16 (hex)
-    .map(function(item){
-        return item.toString(16);
-    });
+        //conver the hex to base 10 (ascii)
+        .map(function (hex) {
+            return parseInt(hex, 16);
+        })
+        //filter out know acceptable characters (ascii)
+        .filter(function (hex) {
+            //if ((hex >= 65 && hex <= 90) || (hex >= 97 && hex <= 122) || (hex >= 48 && hex <= 57) || (hex >= 91 && hex <= 95)) {
 
-    
-    
-    console.log(nastyChars.length, nastyChars);
+            if (
+                //latin chars
+                (hex >= 32 && hex <= 126) ||
+                //newlines
+                (hex == 13 || hex == 10)
+
+            ) {
+                return false;
+            } else {
+                return true;
+            }
+        })
+        //unique the results
+        .filter(function (item, i, daList) {
+            return i == daList.indexOf(item);
+        })
+        //convert value back to to base 16 (hex)
+        .map(function (item) {
+            return {
+                base10: item,
+                base16: item.toString(16),
+                char: String.fromCodePoint(item)
+            };
+        })
+        .sort(function (a, b) {
+            return a.base10 - b.base10;
+        });
+
+
+
+    console.log("bad chars count:", nastyChars.length);
+    console.log("bad Chars:\n", nastyChars);
 
 });
